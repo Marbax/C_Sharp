90e75c7a893a0d748d8830cd621f843e6079ce9f
Marbax <40849771+Marbax@users.noreply.github.com>
2019-10-26 07:59:37 +0300

    hw_02 completed

---
+++ home_work/HW_02/Arrays/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            1.Заполнить одномерный массив случайными числами. Сжать
+            массив, удалив из него все 0 и заполнить освободившиеся
+            справа элементы значениями -1.
+            */
+            Console.WriteLine("zipZeros");
+            int[] arr=null;
+            fillWithRand(out arr,20,0,11);
+            printArr(arr);       
+            zipZeros(arr);
+            printArr(arr);
+            /*=======================================================*/
+            /*=========================_END_=========================*/
+
+            /*
+             2. Заполнить одномерные массив случайными числами.
+             Преобразовать массив так, чтобы сначала шли отрицательные
+             элементы, а потом положительные.
+             */
+            Console.WriteLine("sort");
+            int[] arr1 = null;
+            fillWithRand(out arr1, 20, -10, 11);
+            printArr(arr1);
+            Array.Sort(arr1);
+            printArr(arr1);
+            /*=======================================================*/
+            /*=========================_END_=========================*/
+
+            /*
+            3.В двумерном массиве, размеры которого вводятся с
+            клавиатуры, поменять местами заданные столбцы.
+            */
+            Console.WriteLine("replace Collums");
+            Console.WriteLine("Enter rows count =>");
+            int row = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter cols count =>");
+            int col = Int32.Parse(Console.ReadLine());
+            int[,] tdarr = new int[row, col];
+            fillWithRand(tdarr,0,10);
+            printArr(tdarr);
+            Console.WriteLine("Enter indexes of cullums which you want replace ,separate by the Enter =>");
+            replaceCollums(tdarr,Int32.Parse(Console.ReadLine()),Int32.Parse(Console.ReadLine()));
+            printArr(tdarr);
+            /*=======================================================*/
+            /*=========================_END_=========================*/
+        }
+        static void replaceCollums(int[,]arr,int first_col,int second_col)
+        {
+            for (var i = 0; i < arr.GetLength(0); i++)
+            {
+                (arr[i, first_col], arr[i, second_col]) = (arr[i, second_col], arr[i, first_col]);
+            }
+        }
+
+        static void printArr(int[,] arr)
+        {
+            for (var i = 0; i < arr.GetLength(0); i++)
+            {
+                for (var j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void printArr(int[]arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void fillWithRand(int[,] arr, int start_range, int end_range)
+        {
+            Random rnd = new Random();
+
+            for (var i = 0; i < arr.GetLength(0); i++)
+            {
+                for (var j = 0;j < arr.GetLength(1); j++)
+                {
+                    arr[i,j] = rnd.Next(start_range, end_range);
+                }
+            }
+        }
+
+        static void fillWithRand(out int[] arr,int size,int start_range,int end_range)
+        {
+            arr = new int[size];
+            Random rnd = new Random();
+
+            for (var i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(start_range, end_range);
+            }
+        }
+        static void zipZeros(int[]arr)
+        {
+            for (var i = 0; i < arr.Length; i++)
+            {
+                while (arr[i] == 0)
+                {
+                    for (var j = i; j < arr.Length - 1; j++)
+                    {
+                        arr[j] = arr[j + 1];
+                    }
+                    arr[arr.Length - 1] = -1;
+
+                }
+            }
+        }
+
+    }
+
+}
---
+++ home_work/HW_02/Strings/Program.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             1. Создать метод, принимающий введенную пользователем
+            строку, и выводящий на экран статистику по этой строке.
+            Статистика должна включать общее кол-во символов, кол-во
+            пробелом, кол-во цифр, кол-во символов пунктуации, кол-во
+            букв и кол-во пробелов.
+            */
+            Console.WriteLine("1.Enter String => ");
+            string str = Console.ReadLine();
+            stringStatistick(ref str);
+            Console.WriteLine();
+
+            /*
+             2. Пользователь вводит строку и символ. В строке найти все
+            вхождения этого символа и перевести его в верхний регистр, а
+            также удалить часть строки, начиная с последнего вхождения
+            этого символа и до конца.
+            */
+            Console.WriteLine("2.Enter String => ");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Enter a character to find it in the string and delete everything after its last finding => ");
+            char chr = Char.Parse(Console.ReadLine());
+            charToUpperAndRemoveEnd(ref str1, ref chr);
+
+            /*
+             3. Удалить из строки повторяющиеся слова.
+            */
+            Console.WriteLine("3.Enter String => ");
+            string str2 = Console.ReadLine();
+            //string str2 = "play play not play with not game in";
+            removeReplayedWords(ref str2);
+
+            /*
+             4. Преобразовать все слова по правилу: удалить из слова все
+            последующие вхождения первого символа.
+            */
+            Console.WriteLine("4.Enter String => ");
+            string str3 = Console.ReadLine();
+            //string str3 = "mammba karamkba trultttulu lalala";
+            removeReplayedChars(ref str3);
+
+
+            /*
+             5. Преобразовать все слова по правилу: удалить в слове только
+            последние вхождения каждого символа.
+            */
+            Console.WriteLine("5.Enter String => ");
+            string str4 = Console.ReadLine();
+            //string str4 = "mammba. karamkba, trultttulu lalala";
+            removeLastCharOccurrence(ref str4);
+
+        }
+
+
+        static void stringStatistick(ref string str)
+        {
+            int spaces = 0, digits = 0, punctuations = 0, letters = 0, let_a_spa = 0;
+            char[] buf = str.ToCharArray();
+            char[] digits_arr = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            char[] punctuations_arr = new char[] { ',', '.', '\'', '\"', '\\','/',':',';','_','{','}',
+                '?','!','(',')','{','}','[',']','*'};
+            foreach (var sym in buf)
+            {
+                if (sym == ' ')
+                {
+                    spaces++;
+                }
+                else if (digits_arr.Contains(sym))
+                {
+                    digits++;
+                }
+                else if (punctuations_arr.Contains(sym))
+                {
+                    punctuations++;
+                }
+            }
+            let_a_spa = str.Count() - digits - punctuations;
+            letters = let_a_spa - spaces;
+            Console.WriteLine($"symbols in string {str.Count()}\nspaces {spaces}");
+            Console.WriteLine($"digits {digits}\npunctuations {punctuations}\nletters {letters}\nletters and spaces {let_a_spa} ");
+
+        }
+
+        static List<int> charToUpperAndRemoveEnd(ref string str, ref char chr)
+        {
+            StringBuilder buf_str = new StringBuilder("");
+            int counter = 0;
+            List<int> positions = new List<int>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == chr)
+                {
+                    positions.Add(i);
+                    counter++;
+                    buf_str.Append(Char.ToUpper(chr)); ;
+                }
+                else
+                    buf_str.Append(str[i]);
+            }
+            str = buf_str.ToString();
+            if (counter > 1)
+            {
+                str = str.Remove(positions.Last());
+            }
+            Console.WriteLine($"Edited string => \n{str}\nPositions of a {chr} in the primal string =>");
+            foreach (var i in positions)
+                Console.Write(i + " ");
+
+            Console.WriteLine();
+            return positions;
+        }
+
+        static void removeReplayedWords(ref string str)
+        {
+            string[] str_arr = str.Split(",. :/?!;".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder buf_str = new StringBuilder("");
+            for (int i = 0; i < str_arr.Length; i++)
+            {
+
+                if (!buf_str.ToString().Contains(str_arr[i]))
+                    buf_str.Append(str_arr[i]);
+                if (i < str_arr.Length - 1)
+                    buf_str.Append(' ');
+
+            }
+            str = buf_str.ToString();
+            Console.WriteLine(str);
+
+        }
+
+        static void removeReplayedChars(ref string str)
+        {
+            string[] str_arr = str.Split(",. :/?!;".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder buf_str = new StringBuilder("");
+            StringBuilder str_concat = new StringBuilder("");
+            for (int i = 0; i < str_arr.Length; i++)
+            {
+                buf_str.Append(str_arr[i][0]);
+                for (int j = 1; j < str_arr[i].Length; j++)
+                {
+                    if (str_arr[i][j] != (str_arr[i][0]))
+                        buf_str.Append(str_arr[i][j]);
+                }
+
+                if (i < str_arr.Length - 1)
+                    buf_str.Append(' ');
+
+                str_concat.Append(buf_str);
+                buf_str.Clear();
+
+            }
+
+            str = str_concat.ToString();
+            Console.WriteLine(str);
+
+        }
+
+        static void removeLastCharOccurrence(ref string str)
+        {
+            string[] str_arr = str.Split(",. :/?!;".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder buf_str = new StringBuilder("");
+            StringBuilder str_concat = new StringBuilder("");
+            for (int i = 0; i < str_arr.Length; i++)
+            {
+                for (int j = 0; j < str_arr[i].Length; j++)
+                {
+                    int counter_next = 0;
+                    for (int k = j+1; k < str_arr[i].Length; k++)
+                    {
+                        if (str_arr[i][j] == str_arr[i][k])
+                            counter_next++;
+                    }
+
+                    int counter_prev = 0;
+                    for (int k = j-1; k >= 0; k--)
+                    {
+                        if (str_arr[i][j] == str_arr[i][k])
+                            counter_prev++;
+                    }
+
+                    if (counter_prev > 0 && counter_next == 0)
+                    {
+                        continue;
+                    }
+                    else
+                        buf_str.Append(str_arr[i][j]);
+
+                }
+
+                if (i < str_arr.Length - 1)
+                    buf_str.Append(' ');
+
+                str_concat.Append(buf_str);
+                buf_str.Clear();
+
+            }
+            str = str_concat.ToString();
+            Console.WriteLine(str);
+
+        }
+    }
+}
