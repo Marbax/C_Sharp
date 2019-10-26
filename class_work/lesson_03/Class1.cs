f4fa54297d3ea9a82ad6e96749e0ae12eb486c52
Marbax <40849771+Marbax@users.noreply.github.com>
2019-10-26 09:26:00 +0300

    cw_03

---
+++ class_work/lesson_03/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_03
+{
+    public class Person
+    {
+        private string _name;
+        private readonly DateTime _birthday;
+        private static int _counter = 0;
+        public const string Planet = "Earth";
+
+
+        public string LastName { get; set; } // публичное поле класса с геттером и сеттером "Автосвойство"
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public static int Counter => _counter; // если только геттер или только сеттер
+
+        public DateTime Birthday => _birthday;
+
+        //public string GetName() => _name; // getter в одну строку
+        //public void SetName(string _name) => this._name = _name; // присвоение значения в одну строку 
+
+        public Person() : this("NONAME", DateTime.Now)// делегирование своему констроктору (текущее время ,до секунды)
+        {
+
+        }
+        public Person(string _name, DateTime birthday)
+        {
+            this._name = _name;// ссылка присваевается ссылке из вне она не поменяется ,т.к. строки неизменны
+            _birthday = birthday;
+            _counter++;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {_name}\n" +
+                $"Surname: {LastName}\n"+
+                $"Birthday: {_birthday.ToLongDateString()}\n" +
+                $"Planet: {Planet}\n" +
+                $"Number of people: {_counter}";
+        }
+    }
+}
---
+++ class_work/lesson_03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person a = new Person("Petro", new DateTime(1999, 07, 25)); // вариант добавления даты
+            Console.WriteLine(a);
+
+            Person b = new Person();
+            Console.WriteLine(b);
+
+            Person c = new Person(birthday: new DateTime(day: 1, month: 07, year: 1992), _name: "Petro"); // именованые параметры
+            Console.WriteLine(c);
+            c.Name = "Fridrih";
+            Console.WriteLine(c.Name);
+            Console.WriteLine(c);
+
+            Console.WriteLine();
+            Console.WriteLine("bday = " + c.Birthday + ", counter = " + Person.Counter + ", Planet = " + Person.Planet);
+            Console.WriteLine();
+
+            c.LastName = "Muppet";
+            Console.WriteLine(c);
+        }
+    }
+}
